2266a7eb1b09605aa57b671a51b97f33646f6189
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-14 11:18:21 -0500

    modification added

--- src/subtration/sub.java
+++ src/subtration/sub.java
@@ -4,7 +4,9 @@ public class sub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("subtraction of this numer");
 System.out.println(70-6);
+
 	}
 
 }
